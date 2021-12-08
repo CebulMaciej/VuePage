8807fc901b4b3055aec53bfa1ea3e1b9602f68cc
Maciej Cebula <mcebula@mobitouch.net>
2021-12-08 21:33:06 +0100

    change first page label

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/myFirstPage">MyFirstPAge</router-link>
+    <router-link to="/myFirstPage">MyFirstPage</router-link>
   </div>
   <router-view />
 </template>
